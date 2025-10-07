707b9ecc4b2eb476a1e42a08e4af3d5a7c69ed94
aishamatak <aisha.matak@ada.ac.uk>
2025-10-07 10:54:40 +0000

    adding a tip

--- 1_temperature_converter.py
+++ 1_temperature_converter.py
@@ -6,3 +6,19 @@ def celsius_to_fahrenheit(celsius):
 print(celsius_to_fahrenheit(0))
 print(celsius_to_fahrenheit(20))
 print(celsius_to_fahrenheit(100))
+
+
+# Changing the function to work out Farenheit into Celsius 
+def farenheit_to_celsius(farenheit): 
+    celsius = (farenheit - 32) / 9/5 
+    return celsius 
+
+# Converting from kilometers to miles 
+def kilometers_to_miles(kilometers):  
+    miles = kilometers * 0.621371
+    return miles 
+
+# Converting from miles to kilometers 
+def miles_to_kilometers(miles):
+    kilometers = miles / 0.621371
+    return kilometers 
--- 2_shopping_calculator.py
+++ 2_shopping_calculator.py
@@ -8,3 +8,12 @@ def calculate_total(price, tax_rate=0.20, discount=0):
 print(f"£{calculate_total(100):.2f}")
 print(f"£{calculate_total(100, 0.1):.2f}")
 print(f"£{calculate_total(100, 0.08, 10):.2f}")
+
+#adding a default 12% tip 
+def calculate_total(price, tax_rate=0.2, discount=0): 
+    subtotal = price - discount 
+    tax = subtotal * tax_rate 
+    total = subtotal + tax 
+    tip = total * 1.12 
+    return tip 
+
